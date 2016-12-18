ca6526acbe433fa31817681c38c8b20ec1900256
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-12-18 14:06:58 +0100

    Download timezone module

--- scripts/downloadPuppetModules.sh
+++ scripts/downloadPuppetModules.sh
@@ -11,6 +11,7 @@ puppet module install puppetlabs/mysql --modulepath $dir --version 3.6.1;
 puppet module install example42-php  --modulepath $dir --version 2.0.25;
 puppet module install puppetlabs-apache --modulepath $dir --version 1.6.0;
 puppet module install crayfishx-firewalld --modulepath $dir --version 1.2.3;
+puppet module install example42-timezone --modulepath $dir --version 2.0.13;
 puppet module install arioch/redis --modulepath $dir --version 1.2.2;
 
 exit 0;
